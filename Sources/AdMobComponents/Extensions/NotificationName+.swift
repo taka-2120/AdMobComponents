65a3815f3c2bd2b9abfea289e23461de874d1c00
taka-2120 <yu_tkhs@icloud.com>
2025-02-23 19:29:32 +0900

    fix: avoid multiple instantiation

---
+++ Sources/AdMobComponents/Extensions/NotificationName+.swift
@@ -0,0 +1,12 @@
+//
+//  NotificationName+.swift
+//  AdMobComponents
+//
+//  Created by Yu Takahashi on 2/23/25.
+//
+
+import Foundation
+
+extension Notification.Name {
+    static let onNetworkStatusChanged = Notification.Name("NetworkStatusChanged")
+}
